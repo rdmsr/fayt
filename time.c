a00ad426a5f89f3025963941ad49ea953975c9aa
Ethan <ethanm4984@gmail.com>
2025-01-11 11:46:07 -0700

    time

---
+++ time.c
@@ -0,0 +1,54 @@
+#include <fayt/time.h>
+
+struct time invariant_tsc_read(struct timer *timer) {
+	struct time ret = { .sec = 0, .nsec = 0 };
+	if(timer == NULL) return ret;
+
+	uint64_t tsc = ({
+		uint64_t rax, rdx;
+		__asm__ volatile ("rdtsc" : "=a"(rax), "=d"(rdx));
+		(uint64_t)rax | ((uint64_t)rdx << 32);
+	});
+
+	uint64_t ns = (tsc * NANO_PER_SECOND) / timer->freq;
+
+	ret = (struct time) {
+		.nsec = ns % NANO_PER_SECOND,
+		.sec = ns / NANO_PER_SECOND
+	};
+
+	return ret;
+}
+
+struct time time_add(struct time a, struct time b) {
+	struct time ret = {
+		.sec = a.sec + b.sec,
+		.nsec = a.nsec + b.nsec
+	};
+
+	if(ret.nsec > NANO_PER_SECOND) {
+		ret.nsec -= NANO_PER_SECOND;
+		ret.sec++;
+	}
+
+	return ret;
+}
+
+struct time time_sub(struct time a, struct time b) {
+	struct time ret = {
+		.nsec = a.nsec - b.nsec,
+		.sec = a.sec - b.sec
+	};
+
+	if(ret.nsec < 0) {
+		ret.nsec = NANO_PER_SECOND - b.nsec - a.nsec;
+		ret.sec--;
+	}
+
+	if(ret.sec < 0) {
+		ret.nsec = 0;
+		ret.sec = 0;
+	}
+
+	return ret;
+}
--- time.h
+++ time.h
@@ -4,10 +4,29 @@
 #include <stdint.h>
 #include <stddef.h>
 
-static inline uint64_t rdtsc(void) {
-	uint64_t rax, rdx;
-	__asm__ volatile ("rdtsc" : "=a"(rax), "=d"(rdx));
-	return (uint64_t)rax | ((uint64_t)rdx << 32);
-}
+#define NANO_PER_SECOND 1000000000
+
+typedef int64_t time_t;
+typedef int64_t freq_t;
+
+struct time {
+	time_t sec;
+	time_t nsec;
+};
+
+#define TIME_SOURCE_INVARIANT_TSC 1
+#define TIME_SOURCE_HPET 2
+
+struct timer {
+	int source;
+	freq_t freq;
+	struct time (*read)(struct timer*);
+};
+
+struct time time_add(struct time, struct time);
+struct time time_sub(struct time, struct time);
+struct time time_convert(struct timer*, time_t);
+
+struct time invariant_tsc_read(struct timer *timer);
 
 #endif
