5c8733158d2bca2284d84efb681250b280828cd4
Ethan <ethanm4984@gmail.com>
2025-01-13 20:39:11 -0700

    common sched structures

--- sched.h
+++ sched.h
@@ -5,8 +5,27 @@ constexpr int SCHED_TICK_RATE_MS = 20;
 constexpr int NOT_SCHED_ENQUEUE = 1;
 constexpr int NOT_SCHED_DEQUEUE = 2;
 
-struct sched_common {
-	int blocked;
+struct sched_descriptor {
+	struct timer timer;
+	int processor_id;
+	int queue_default_refill;
+	int load;
+	int cid;
+	struct time slice;
+};
+
+struct sched_queue_config {
+	int cid;
+	int cgroup;
+	int nice;
+	int offload;
+	int phantom_runtime;
+	int flags;
+};
+
+struct sched_queue_config_set {
+	int cnt;
+	struct sched_queue_config config[];
 };
 
 #endif
