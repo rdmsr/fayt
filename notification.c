251190f7208025621dfa9a42dd96535285b092a4
Ethan <ethanm4984@gmail.com>
2025-01-20 20:58:08 -0700

    notify

--- notification.c
+++ notification.c
@@ -9,6 +9,22 @@
 int notify(struct comm_bridge *bridge) {
 	if(bridge == NULL) return -1;
 
+	uintptr_t vaddr;
+	int ret = as_allocate(&address_space, &vaddr,
+		DIV_ROUNDUP(bridge->data.limit, PAGE_SIZE) * PAGE_SIZE);
+	if(ret == -1) return -1;
+	
+	void *tmp = bridge->data.base;
+	bridge->data.base = (void*)vaddr;
+
+	struct syscall_response response = SYSCALL1(SYSCALL_NOTIFICATION_BUILD, bridge);
+	if(response.ret == -1) return -1;
+
+	memcpy((void*)bridge->data.base, tmp, bridge->data.limit);
+
+	response = SYSCALL1(SYSCALL_NOTIFICATION_BROADCAST, bridge);
+	if(response.ret == -1) return -1;
+
 	return 0;
 }
 
