b3549427d6d3c3096955331527298561f743b08f
Ethan <ethanm4984@gmail.com>
2025-03-02 23:14:35 -0700

    define handles

--- handle.h
+++ handle.h
@@ -8,6 +8,10 @@
 #define HANDLE_ACCESS_READ (1 << 0)
 #define HANDLE_ACCESS_WRITE (1 << 1)
 
+#define HANDLE_AS 0
+#define HANDLE_MSG_INIT 1
+#define HANDLE_MSG_NOTIF 2
+
 typedef uint32_t handle_t;
 
 enum { HANDLE_CLASS_BLK, HANDLE_CLASS_NOTIFICATION_CHANNEL };
