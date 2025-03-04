dfde2aec5a012eb7701e5b3e367e4b710ba8eb48
Ethan <ethanm4984@gmail.com>
2025-03-03 22:29:13 -0700

    handles

--- handle.h
+++ handle.h
@@ -8,24 +8,9 @@
 #define HANDLE_ACCESS_READ (1 << 0)
 #define HANDLE_ACCESS_WRITE (1 << 1)
 
-#define HANDLE_AS 0
-#define HANDLE_MSG_INIT 1
-#define HANDLE_MSG_NOTIF 2
-
 typedef uint32_t handle_t;
 
+enum { CAPABILITY_SELF_AS, CAPABILITY_SELF_SCHED };
 enum { HANDLE_CLASS_BLK, HANDLE_CLASS_NOTIFICATION_CHANNEL };
-
-struct blk_hdl {
-	size_t lba_start;
-	size_t lba_cnt;
-	size_t lba_size;
-
-	void *private;
-};
-
-struct notif_channel_hdl {
-	struct sched_proc_id proc_id;
-};
 
 #endif
